4b69ad39f06c25a77b713e9203995c5eafe149e6
lanke-01 <lanke@gmx.de>
2026-02-17 17:34:21 +0100

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 import psycopg2
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 
 def get_conn():
@@ -9,12 +10,11 @@ def get_conn():
             dbname=os.getenv("DB_NAME", "hausverwaltung"),
             user=os.getenv("DB_USER", "postgres"),
             password=os.getenv("DB_PASS", ""),
-            host=os.getenv("DB_HOST", "127.0.0.1"),
+            host=os.getenv("DB_HOST", "127.0.0.1"), # WICHTIG
             port=os.getenv("DB_PORT", "5432")
         )
-       
         conn.set_client_encoding('UTF8')
         return conn
     except Exception as e:
-        print(f"Verbindung zur Datenbank fehlgeschlagen: {e}")
+        print(f"Verbindung fehlgeschlagen: {e}")
         return None
