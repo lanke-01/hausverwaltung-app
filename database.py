e56db432184df2ac8b307337e3cf7d3ecbe7bfdb
lanke-01 <lanke@gmx.de>
2026-02-22 13:29:23 +0100

    Update database.py

--- database.py
+++ database.py
@@ -4,15 +4,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+def get_db_config():
+    """Gibt die Konfiguration als Dictionary zurück, ohne Passwörter im Code zu hardcoden."""
+    return {
+        "dbname": os.getenv("DB_NAME", "hausverwaltung"),
+        "user": os.getenv("DB_USER", "postgres"),
+        "password": os.getenv("DB_PASS", ""),
+        "host": os.getenv("DB_HOST", "127.0.0.1"),
+        "port": os.getenv("DB_PORT", "5432")
+    }
+
 def get_conn():
     try:
-        conn = psycopg2.connect(
-            dbname=os.getenv("DB_NAME", "hausverwaltung"),
-            user=os.getenv("DB_USER", "postgres"),
-            password=os.getenv("DB_PASS", ""),
-            host=os.getenv("DB_HOST", "127.0.0.1"),
-            port=os.getenv("DB_PORT", "5432")
-        )
+        config = get_db_config()
+        conn = psycopg2.connect(**config)
         conn.set_client_encoding('UTF8')
         return conn
     except Exception as e:
