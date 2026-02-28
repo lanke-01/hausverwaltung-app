905b0a318de5eb9f0f0e5741e3b0fd9365efe3c2
lanke-01 <lanke@gmx.de>
2026-02-28 15:58:06 +0100

    Update setup_lxc.sh

--- install/setup_lxc.sh
+++ install/setup_lxc.sh
@@ -34,10 +34,9 @@ sleep 10
 pct exec $CTID -- apt update
 pct exec $CTID -- apt install -y git python3 python3-venv python3-pip postgresql postgresql-contrib libpq-dev build-essential
 
-# 4. Git Projekt klonen
-# ERSETZE HIER 'DEINE_REPO_URL' durch deine tatsächliche URL!
-GIT_URL="https://github.com/lanke-01/hausverwaltung.git"
-pct exec $CTID -- git clone $GIT_URL /opt/hausverwaltung
+# 4. GitHub Projekt laden
+pct exec $CTID -- bash -c "git clone https://github.com/lanke-01/hausverwaltung-app.git /opt/hausverwaltung"
+
 
 # 5. Datenbank initialisieren
 echo "--- Datenbank wird konfiguriert ---"
